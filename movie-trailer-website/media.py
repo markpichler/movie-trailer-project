9b6053c6fec792961241813e5d66a149e58874b8
Mark Pichler <mark_pichler@yahoo.com>
2017-08-16 19:58:26 -0700

    Update media.py

--- movie-trailer-website/media.py
+++ movie-trailer-website/media.py
@@ -1,6 +1,5 @@
 class Movie():
     """Defines the basic properties of a movie.
-
     Attributes:
         title (str): Movie title.
         overview (str): Description of movie.
@@ -10,8 +9,6 @@ class Movie():
     def __init__(self, title, overview, poster, trailer):
         self.title = title
         self.overview = overview
-        self.poster_img_url = "http://image.tmdb.org/t/p/original" + str(poster)
+        self.poster_img_url = ("http://image.tmdb.org/t/p/original"
+                               str(poster))
         self.trailer_youtube_url = trailer
-        
-        
-        
